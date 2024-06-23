e21b0f677f5201ca9f5856a1909f3914f9e7b39a
Radik Biktagirov <radikbiktagirov355@gmail.com>
2024-06-24 03:47:13 +0500

    Added Skills Page Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import './App.css';
-import styled from 'styled-components';
-import { Header } from './layout/header/Header';
-import { Main } from './layout/main/Main';
-
+import "./App.css";
+import styled from "styled-components";
+import { Header } from "./layout/header/Header";
+import { Main } from "./layout/main/Main";
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <Main />
-        </div>
-    );
+  return (
+    <div className="App">
+        <Header />
+        <Main />
+    </div>
+  );
 }
 
 export default App;
-
