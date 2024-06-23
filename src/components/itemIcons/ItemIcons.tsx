8d5790dfccfa0556944f82374bd3d331f3377995
Radik Biktagirov <radikbiktagirov355@gmail.com>
2024-06-24 03:06:32 +0500

    Added Skills Page Section

---
+++ src/components/itemIcons/ItemIcons.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import IconsSprite from '../../../src/assets/img/IconsSprite.svg'
+import styled from "styled-components";
+
+type IconPropsType = {
+    iconId: string,
+    width?: string,
+    height?: string,
+    viewBox?: string,
+}
+
+const StyledIconReact = styled.svg`
+    object-fit: cover;
+`
+
+const StyledIconFrontend = styled.svg`
+    object-fit: cover;
+`
+
+export const IconTypeScript = (props: IconPropsType) => {
+    return (
+        <StyledIconReact width= {props.width || "120"}  height={props.height || "110"} viewBox={props.viewBox || "0 0 137 137"} fill="none" xmlns="http://www.w3.org/2000/svg">
+            <use xlinkHref={`${IconsSprite}#${props.iconId}`} />
+        </StyledIconReact>
+    )
+}
+
+export const IconFrontend = (props: IconPropsType) => {
+    return (
+        <StyledIconFrontend width= {props.width || "75"}  height={props.height || "110"} viewBox={props.viewBox || "0 0 75 45"} fill="none" xmlns="http://www.w3.org/2000/svg">
+            <use xlinkHref={`${IconsSprite}#${props.iconId}`} />
+        </StyledIconFrontend>
+    )
+}
+
+export const IconCSS = (props: IconPropsType) => {
+    return (
+        <StyledIconFrontend width= {props.width || "104"}  height={props.height || "110"} viewBox={props.viewBox || "0 0 104 90"} fill="none" xmlns="http://www.w3.org/2000/svg">
+            <use xlinkHref={`${IconsSprite}#${props.iconId}`} />
+        </StyledIconFrontend>
+    )
+}
+
+export const IconReact = (props: IconPropsType) => {
+    return (
+        <StyledIconFrontend width= {props.width || ""}  height={props.height || "113"} viewBox={props.viewBox || "0 0 23 20"} fill="none" xmlns="http://www.w3.org/2000/svg">
+            <use xlinkHref={`${IconsSprite}#${props.iconId}`} />
+        </StyledIconFrontend>
+    )
+}
+
+export const IconHTML = (props: IconPropsType) => {
+    return (
+        <StyledIconFrontend width= {props.width || "100"}  height={props.height || "113"} viewBox={props.viewBox || "0 0 28 20"} fill="none" xmlns="http://www.w3.org/2000/svg">
+            <use xlinkHref={`${IconsSprite}#${props.iconId}`} />
+        </StyledIconFrontend>
+    )
+}
+
+export const IconJS = (props: IconPropsType) => {
+    return (
+        <StyledIconFrontend width= {props.width || "100"}  height={props.height || "113"} viewBox={props.viewBox || "0 0 24 20"} fill="none" xmlns="http://www.w3.org/2000/svg">
+            <use xlinkHref={`${IconsSprite}#${props.iconId}`} />
+        </StyledIconFrontend>
+    )
+}
--- src/layout/main/Main.tsx
+++ src/layout/main/Main.tsx
@@ -2,13 +2,22 @@ import React from "react";
 import { BackgroundImageHeroPage } from "../../components/backgroundImageHeroPage/BackgroundImageHeroPage";
 import styled from "styled-components";
 import photo from "../../assets/img/i_m.webp";
+import {
+  IconCSS,
+  IconFrontend,
+  IconHTML,
+  IconJS,
+  IconReact,
+  IconTypeScript,
+} from "../../components/itemIcons/ItemIcons";
 
-const StyledMainPage = styled.div`
+const StyledHeroPage = styled.div`
+  width: 1440px;
   display: flex;
-  height: 88vh;
+  height: 716px;
   align-items: center;
   background-color: #2c2c2c;
-  padding: 40px 160px 0px 160px;
+  padding: 40px 0px 0px 160px;
   gap: 100px;
 `;
 
@@ -47,35 +56,96 @@ const StyledImage = styled.img`
 `;
 
 const StyledButton = styled.button`
-    background-color: #E2A300;
-    color: #000;
-    padding: 10px 35px;
-    font-size: 18px;
-    font-family: 'NEXT ART', sans-serif;
-    font-weight: 700;
-    margin-top: 20px;
-    border-radius: 6px;
-    box-shadow: 0px 2px 13px 0px rgba(226,158,0,0.45);
-    pointer-events: none;
-`
+  background-color: #e2a300;
+  color: #000;
+  padding: 10px 35px;
+  font-size: 18px;
+  font-family: "NEXT ART", sans-serif;
+  font-weight: 700;
+  margin-top: 20px;
+  border-radius: 6px;
+  box-shadow: 0px 2px 13px 0px rgba(226, 158, 0, 0.45);
+  pointer-events: none;
+`;
+
+const StyledItem = styled.div`
+  width: 230px;
+  height: 230px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #2a2a2a;
+  border-radius: 25px;
+  box-shadow: -1px 11px 12px -4px rgba(0, 0, 0, 0.75);
+`;
+
+const StyledNameItem = styled.h3`
+  font-size: 24px;
+  margin: 25px 0px 0px 0px;
+  font-family: Arodora Pro, serif;
+  color: #fff;
+`;
+
+const StyledSkills = styled.div`
+  width: 1440px;
+  background-color: #414141;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 73px;
+  justify-content: center;
+  padding: 90px 0px;
+`;
 
 export const Main = () => {
   return (
-    <StyledMainPage>
-      <StyledTitle>
-        <span>HELLO</span>
-        <h2>I’M RADIK BIKTAGIROV</h2>
-        <h1>
-          I've been doing web design, front-end development for a six month now.
-          Do you need a website , site layout, or maybe a turnkey website? Then
-          contact me
-        </h1>
-        <StyledButton>CONTACT ME</StyledButton>
-      </StyledTitle>
-      <div>
-        <BackgroundImageHeroPage iconId={"backgroundHomePage"} />
-        <StyledImage src={photo} />
-      </div>
-    </StyledMainPage>
+    <main>
+      <StyledHeroPage>
+        <StyledTitle>
+          <span>HELLO</span>
+          <h2>I’M RADIK BIKTAGIROV</h2>
+          <h1>
+            I've been doing web design, front-end development for a six month
+            now. Do you need a website , site layout, or maybe a turnkey
+            website? Then contact me
+          </h1>
+          <StyledButton>CONTACT ME</StyledButton>
+        </StyledTitle>
+        <div>
+          <BackgroundImageHeroPage iconId={"backgroundHomePage"} />
+          <StyledImage src={photo} />
+        </div>
+      </StyledHeroPage>
+      <StyledSkills>
+        <StyledItem>
+          <IconTypeScript iconId={"reactIcon"} />
+          <StyledNameItem>Type Script</StyledNameItem>
+        </StyledItem>
+        <StyledItem>
+          <IconFrontend iconId={"frontend"} />
+          <StyledNameItem>Frontend</StyledNameItem>
+        </StyledItem>
+        <StyledItem>
+          <IconCSS iconId={"cssIcon"} />
+          <StyledNameItem>CSS</StyledNameItem>
+        </StyledItem>
+        <StyledItem>
+          <IconReact iconId={"tag"} />
+          <StyledNameItem>React</StyledNameItem>
+        </StyledItem>
+        <StyledItem>
+          <IconHTML iconId={"html5"} />
+          <StyledNameItem>HTML</StyledNameItem>
+        </StyledItem>
+        <StyledItem>
+          <IconJS iconId={"javaScript"} />
+          <StyledNameItem>JavaScript</StyledNameItem>
+        </StyledItem>
+        <StyledItem>
+          <IconCSS iconId={"cssIcon"} />
+          <StyledNameItem>CSS</StyledNameItem>
+        </StyledItem>
+      </StyledSkills>
+    </main>
   );
 };
